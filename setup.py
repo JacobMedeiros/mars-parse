3f33ada4a5ec064c1fbb7e523c6b536ae678bedc
Jacob Medeiros <44653498+JacobMedeiros@users.noreply.github.com>
2023-02-01 18:31:02 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='mars_parse',
+    version='0.1.0',
+    author='Jacob Medeiros',
+    author_email='jacobmedeiros16@gmail.com'
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['typer','rich'],
+    license = 'MIT',
+)
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+__app_name__ = "mars-parse"
+__version__ = "0.1.0"
+
